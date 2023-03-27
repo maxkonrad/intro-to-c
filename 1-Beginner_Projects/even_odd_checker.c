128f04f6d25c4201c28ba1d944eea0fc48f3de3d
maxkonrad <furkanselek@outlook.com>
2023-03-27 16:22:01 +0300

    add old solution

--- 1-Beginner_Projects/even_odd_checker.c
+++ 1-Beginner_Projects/even_odd_checker.c
@@ -8,6 +8,14 @@ int main()
 	printf("\nEnter an integer number : ");
 	scanf("%d",&number);
 	
-	printf("\nnumber %d is %s\n", number, (number & 1 ? "odd" : "even")); //very special thanks to thanks u/thebatmanandrobin from r/cprogramming
+	//old solution
+	if (number %2 == 0)
+		printf("\nnumber %d  is even\n", number);
+		
+    if (number %2 == 1)
+		printf("\nnumber %d  is odd\n", number);	
 
+	//optimized solution
+	printf("\nnumber %d is %s\n", number, (number & 1 ? "odd" : "even")); //very special thanks to thanks u/thebatmanandrobin from r/cprogramming	
+	
 }
