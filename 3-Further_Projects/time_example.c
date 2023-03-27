b67c3acc4763d77f67e33dc1db1a1318ec8381cc
maxkonrad <furkanselek@outlook.com>
2023-03-27 08:42:57 +0300

    fix time_example

--- 3-Further_Projects/time_example.c
+++ 3-Further_Projects/time_example.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 
-main(){
+int main(){
 	clock_t start, end;
 	start = clock();
 	int i = 0;
 	end = clock();
-	double time_passed = (double) (end - start);
+	double time_passed = ((double) (end - start)) / CLOCKS_PER_SEC;
 	printf("time passed = %f", time_passed);
 }
