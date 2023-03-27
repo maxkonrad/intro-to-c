ab937fa48b09388dc498f07ff017196a723eb923
maxkonrad <furkanselek@outlook.com>
2023-03-27 16:17:33 +0300

    optimize even_odd_checker, add reddit feedbacks

--- 1-Beginner_Projects/even_odd_checker.c
+++ 1-Beginner_Projects/even_odd_checker.c
@@ -8,10 +8,6 @@ int main()
 	printf("\nEnter an integer number : ");
 	scanf("%d",&number);
 	
-	if (number %2 == 0)
-		printf("\nnumber %d  is even", number);
-		
-    if (number %2 == 1)
-		printf("\nnumber %d  is odd", number);		
-	
+	printf("\nnumber %d is %s\n", number, (number & 1 ? "odd" : "even")); //very special thanks to thanks u/thebatmanandrobin from r/cprogramming
+
 }
